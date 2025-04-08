68743b50fc45f7d247be6b4e1e854602d7a4dc59
wintonml <wintonml@gmail.com>
2025-04-08 19:02:13 +0100

    Update display information

--- my-cool-site/src/App.js
+++ my-cool-site/src/App.js
@@ -3,21 +3,21 @@ import BlogList from './components/BlogList/BlogList';
 import './App.css';
 
 function App() {
-  const samplePosts = [
+  const Author = 'Michael Lennon Winton';
+  const PlaceHolderPosts = [
     {
       title: 'Welcome to My Blog',
       content:
         "This is my first blog post. I'm excited to share my thoughts and experiences with you.",
       date: '2024-04-06',
-      author: 'Your Name',
+      author: Author,
       tags: ['welcome', 'introduction'],
     },
     {
       title: 'Getting Started with React',
-      content:
-        "React is a powerful JavaScript library for building user interfaces. In this post, we\'ll explore the basics.",
+      content: 'React is a powerful JavaScript library for building user interfaces.',
       date: '2024-04-07',
-      author: 'Your Name',
+      author: Author,
       tags: ['react', 'javascript', 'web-development'],
     },
   ];
@@ -25,10 +25,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Personal Blog</h1>
+        <h1>Mi-cool</h1>
       </header>
       <main>
-        <BlogList posts={samplePosts} />
+        <BlogList posts={PlaceHolderPosts} />
       </main>
     </div>
   );
