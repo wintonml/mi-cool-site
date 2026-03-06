7865ef1c900ab491341f39c2ea6d2553ebc4f20b
wintonml <wintonml@gmail.com>
2026-03-06 22:40:03 +0000

    Updates

---
+++ blog-posts.js
@@ -0,0 +1,5 @@
+// This file is auto-generated. Do not edit directly.
+// It contains all blog posts for the static site.
+window.__BLOG_POSTS__ = {
+  "hello-blog.md": "---\ntitle: \"Hello Blog\"\ndate: \"2026-03-05\"\nauthor: \"Lennon Winton\"\ntags: [\"welcome\", \"introduction\"]\nexcerpt: \"This is my first blog post on my new site!\"\n---\n\n# Welcome to My Blog\n\nThis is my first blog post using my new site! I'm excited to start sharing my thoughts and all that jazz!\n\n## Why I Started This Blog\n\nThe idea of content creation is something that has always intrigued me and having my own space where I can share what I've been getting up to, seems cool to me. Especially, if I've built it myself.\nAnother reason for this is I want to improve my frontend dev skills as it is something I am doing more frequently at work, and I think this will be a fun way to learn and improve.\n\nStay tuned for more content!\n"
+};
