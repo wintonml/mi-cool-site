3b8d5f8fc869817450e19187e1fea1657047bcd9
Michael Lennon Winton <39248842+wintonml@users.noreply.github.com>
2026-05-03 15:13:12 +0100

    chore : 44 : Remove the About page (#51)
    ## 📝 Description
    Removing the About page.
    
    Fixes #44 
    
    ---
    
    ## 🔄 Type of Change
    
    <!-- Please delete options that are not relevant. -->
    
    - [ ] 🐛 Bug fix (non-breaking change that fixes an issue)
    - [ ] 🚀 New feature (non-breaking change that adds functionality)
    - [ ] 💥 Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [x] 🧹 Code cleanup or refactor
    - [ ] 🧪 Tests
    - [ ] 📘 Documentation update
    
    ---
    
    ## 🧪 How Has This Been Tested?
    
    <!-- Describe how you tested your changes and what test cases were used.
    -->
    
    - [ ] Unit tests
    - [ ] Integration tests
    
    ---
    
    ## 🧩 Screenshots / Visuals (if applicable)
    
    <!-- Add screenshots or GIFs to help reviewers understand the changes
    visually. -->
    
    ---
    
    ## ✅ Checklist
    
    <!-- Make sure your PR meets the following criteria -->
    
    - [x] My code follows the project’s style guidelines
    - [x] I have performed a self-review of my code
    - [x] I have commented my code where necessary
    - [x] I have updated documentation (if needed)
    - [x] I have added tests that prove my fix/feature works
    - [x] Existing tests pass locally
    
    ---
    
    ## 📚 Additional Notes
    
    <!-- Add any other context, dependencies, or information reviewers
    should know. -->

--- my-cool-site/src/pages/About.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PlaceholderPage from '../components/PlaceholderPage/PlaceholderPage';
-
-const About: React.FC = () => {
-  return <PlaceholderPage title="About" />;
-};
-
-export default About;
