0afb21aa478eaa6e9c02a2db7df19f66abf8e9b2
wintonml <wintonml@gmail.com>
2026-05-04 02:02:16 +0100

    Updating site navigation

--- my-cool-site/src/App.tsx
+++ my-cool-site/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Project from './pages/Project';
