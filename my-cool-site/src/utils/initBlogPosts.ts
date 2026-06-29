08429894643efe27143d94b5e15ae5f29d70afaf
wintonml <wintonml@gmail.com>
2026-06-29 12:07:57 +0100

    Removing debugging messages

--- my-cool-site/src/utils/initBlogPosts.ts
+++ my-cool-site/src/utils/initBlogPosts.ts
@@ -1,5 +1,5 @@
 // This file initializes the blog posts in the browser
-import { initializeBlogPosts, BlogPost, getAllPosts } from './markdownUtils';
+import { initializeBlogPosts, BlogPost } from './markdownUtils';
 
 declare global {
   interface Window {
@@ -11,13 +11,14 @@ export function initBlogPosts() {
   // Always use the same posts data regardless of environment
   const postsToUse = window.__BLOG_POSTS__;
 
-  console.log(`Initializing blog posts in ${process.env.NODE_ENV} mode`);
-  console.log('Available posts:', Object.keys(postsToUse || {}));
+  // Useful for debugging
+  // console.log(`Initializing blog posts in ${process.env.NODE_ENV} mode`);
+  // console.log('Available posts:', Object.keys(postsToUse || {}));
 
   initializeBlogPosts(postsToUse || {});
 
   // Log the posts after initialization for debugging
-  console.log('After initialization - getAllPosts() result:', getAllPosts());
+  // console.log('After initialization - getAllPosts() result:', getAllPosts());
 }
 
 // Initialize immediately when this module is imported
